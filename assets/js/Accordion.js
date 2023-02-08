62bf82c24c287fc64d8a16e61eb6f1efc471d160
Jamil-Arifoglu <jamilai@code.edu.az>
2023-02-08 09:37:27 +0400

    Accordiontask

---
+++ assets/js/Accordion.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  
+    let $btn=$("button")
+    let $lorem=$(".lorem")
+    let styl={
+        padding: "10px",
+        display: "none"
+    }
+    let style = {
+        width: "100%",
+        height: "50px",
+        backgroundColor: "grey"
+    }
+    $btn.css(style);
+    $lorem.css(styl);
+    $(".btn").click(function () {
+      $(this).next().slideToggle();
+      $(".lorem").not($(this).next()).slideUp();
+    });
+});
+// $(document).ready(function(){
+//     $(".btn").click(function(){
+//         let lorem=$(this).next();
+//         $(lorem).toggleClass("lorem");
+//         if($(lorem).hasClass("lorem")){
+//             $(lorem).slideDown();
+//         }
+//         else{
+//             $(lorem).slideUp();
+//         }
+//     });
+// });
