362076d78674b1950521c64a17663ff3ac145872
Jamil-Arifoglu <jamilai@code.edu.az>
2023-02-08 12:24:18 +0400

    Accordiontask

--- assets/js/Accordion.js
+++ assets/js/Accordion.js
@@ -1,22 +1,29 @@
-$(document).ready(function(){
-  
-    let $btn=$("button")
-    let $lorem=$(".lorem")
-    let styl={
-        padding: "10px",
-        display: "none"
-    }
-    let style = {
-        width: "100%",
-        height: "50px",
-        backgroundColor: "grey"
-    }
-    $btn.css(style);
-    $lorem.css(styl);
-    $(".btn").click(function () {
-      $(this).next().slideToggle();
-      $(".lorem").not($(this).next()).slideUp();
-    });
+$(document).ready(function () {
+  let $btn = $("button");
+  let $lorem = $(".lorem");
+  let icon = $("i");
+
+ 
+  let styl = {
+    padding: "10px",
+    display: "none",
+  };
+  let style = {
+    width: "100%",
+    height: "50px",
+    backgroundColor: "grey",
+  };
+  $btn.css(style);
+  $lorem.css(styl);
+
+
+  $(".btn").click(function () {
+    $(icon).toggleClass("active");
+   
+    $(this).next().slideToggle();
+    $(".lorem").not($(this).next()).slideUp();
+    
+  });
 });
 // $(document).ready(function(){
 //     $(".btn").click(function(){
